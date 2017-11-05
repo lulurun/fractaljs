a383117d1be5c3851a91e25d2af021f52f474d73
lulurun <lulurun@gmail.com>
2017-11-05 23:24:27 +0900

    example/onsenui forms

--- examples/onsenui/src/component/forms.js
+++ examples/onsenui/src/component/forms.js
@@ -1,1 +1,18 @@
-F.component('forms', {}, 'Base');
+F.component('forms', {
+  data: {
+    text_input_value: '',
+    switches_value: true,
+    select_value: 'FractalJS',
+    radio_value: 'Oranges',
+    checkbox_value: ['GREEN', 'BLUE'],
+    volume_value: 25,
+    volume_loud: false,
+  },
+  rendered: function(cb) {
+    this.ractive.observe('volume_value', (newVal, oldVal) => {
+      console.log(newVal, oldVal);
+      this.ractive.set('volume_loud', newVal > 80);
+    });
+    cb();
+  }
+}, 'Base');
